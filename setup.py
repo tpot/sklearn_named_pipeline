250188624f9b1c5570b0f41561ee540cddfcd1bc
Tim Potter <tpot@frungy.org>
2023-02-05 10:53:06 +1100

    Create Python packaging for classes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='sklearn_named_pipeline',
+	version='0.1',
+	description='sklearn utility classes',
+	author='Nick Potter',
+	author_email='nobody@example.com',
+	packages=['sklearn_named_pipeline'],
+	install_requires=[
+            'scikit-learn',
+	],
+)
